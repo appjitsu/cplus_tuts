819ad4ccd5f7046fbfcebd723ea4d5cb7e9e1fd9
Jason Cochran <jlcochran2013@gmail.com>
2021-11-22 21:39:34 -0600

    init commit

---
+++ favorite_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int favorite_namber;
+
+    std::cout << "Enter your favorite number between 1 and 100: " << std::endl;
+
+    std::cin >> favorite_namber;
+
+    std::cout << "Amazing!! " << favorite_namber << " is my favorite too!" << std::endl;
+    
+    return 0;
+}
